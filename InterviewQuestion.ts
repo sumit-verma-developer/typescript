4d8ea82ad0b060c82cdb4ff8f6e6e3f23fed1295
operation-tss <tssoperation@gmail.com>
2025-04-24 17:56:22 +0530

    fixed

---
+++ InterviewQuestion.ts
@@ -0,0 +1,49 @@
+// InterView question
+
+// what is a TypeScript and how does it differ from javascript?
+// ans-  TypeScript  is a superset of javascript that adds static types.
+// its Provides type safety and better tooling support compared to Javascript.
+
+// what are the basic type in typeScript?
+// Basic types include number, string, boolean, null, undefined , void , unknow, and any.
+
+// how do you declare a variable with a specific type in TypeScript.
+
+// let age:number=30
+
+// what is an interface in typeScript?
+// An interface is a way to define a contract for classes or objects, specifying what properties and methods they should have.
+
+// what is the purpose of type and interface in typescript?
+// Both type and interface are used to define shapes of objects and functions.
+//Interfaces are preferred for defining object structures and extanding class,
+// while type is more flexible and can defined complex union  and intersection types.
+
+// what are typeScript enums and how are they used?
+// Enums are a way to define a set of named constants.
+// They can be numeric or string-based.
+
+
+// explanin the concept of union types in typeScript.
+// Union types allow a variable to be one of several types.
+// Example: let value :number | string.
+
+// how do you handle optional properties in typeScript?
+// optioanl properties are decleared with a question mark?
+// after the property name. 
+//Example:- interface User { name:string,age?:number}
+
+
+
+// what are generics in typeScript and why are they useful?
+// Generics allow you to create reusable components that can work with any data type.
+// they help in maintaining type safety while writing reusable code.
+
+
+
+// what is type inference in typescript?
+// Type inference is TypeScript's ability to automatically deduce the tye of a variable based on its value.
+
+
+// what are the gaurds and how do they work?
+// they gaurds are functions or constructs that help TypeScript narrow down the type of a 
